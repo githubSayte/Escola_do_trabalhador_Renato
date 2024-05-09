5b33bdd212b071dbc1f154f559100b796119656a
Hokague <criativasayte@gmail.com>
2024-05-09 11:41:50 -0300

    n2

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
- ' use strict'
- const switcher = document.querySelector('btn');
- 
- switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
+'use strict';
 
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.TextContent = "Dark"
-    }
-    else {
-        this.TextContent = "Light"
-    }
+const switcher = document.querySelector('.btn'); // Correção: Adicionado "." antes de "btn"
+
+switcher.addEventListener('click', function() {
+ document.body.classList.toggle('dark-theme');
+
+var className = document.body.className;
+ if (className === "light-theme") { // Use "===" para comparação estrita
+ this.textContent = "Dark"; // Correção: "textContent" em minúsculas
+ } else {
+ this.textContent = "Light"; // Correção: "textContent" em minúsculas
+ }
 });
-
-
 
